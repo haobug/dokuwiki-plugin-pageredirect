c2c87101bae907bfe56dd7e9179ab432b87aa9b7
haobug <qingxianhao@gmail.com>
2013-05-31 14:04:12 +0800

    add Simplified Chinese strings

---
+++ lang/zh/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * English language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     David Lorentsen <zyberdog@quakenet.org>
+ */
+
+$lang['redirect_to']     = "页面已重定向到%s。";
+$lang['redirected_from'] = "本页面重定向自%s。";
---
+++ lang/zh/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * English language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     David Lorentsen <zyberdog@quakenet.org>
+ */
+
+$lang['show_note'] = '在重定向目标页面上显示提示？';
